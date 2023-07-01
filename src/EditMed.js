3cd72c5fbe1080011862a312e63b637b5e41385d
Philip Dimaano <pdimaano7@gmail.com>
2023-07-01 11:52:48 -0700

    initial functions for editing, deleting and updating a medication

--- src/EditMed.js
+++ src/EditMed.js
@@ -12,3 +12,23 @@ import MedForm from "./MedForm";
  *  EditableMedList -> EditableMed -> { Med, MedForm }
  *
  */
+
+function EditableMed({ med, remove, update }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  /** Toggle if this is being edited */
+  function toggleEdit() {
+    setIsEditing(edit => !edit);
+  }
+
+  /** Call remove fn passed to this */
+  function handleDelete() {
+    return remove(todo.id);
+  }
+
+  /** Edit form saved; toggle is Editing and update in ancestor */
+  function handleSave(formData) {
+    update({ id: med.id, ...formData });
+    setIsEditing(false);
+  }
+}
