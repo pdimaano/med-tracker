ac9ba55c7c881cb6e72abc581e81908743c7cf44
Philip Dimaano <pdimaano7@gmail.com>
2023-07-05 13:44:11 -0700

    added useState and functions for creating, updated and removing medication from list

--- src/MedApp.js
+++ src/MedApp.js
@@ -14,3 +14,30 @@ import EditMedList from "./EditMedList";
  *
  * App -> MedApp -> { MedForm, EditMedList }
  */
+
+function MedApp({ initialMeds }) {
+  const [meds, setMeds] = useState(initialMeds);
+
+  console.log("MedApp", initialMeds, meds);
+
+  /** Add a new medication to list */
+  function create(newMed) {
+    setMeds(meds => [...meds, { id: uuid(), ...newMed }]);
+  }
+
+  /** Updated a medication with updatedMed */
+  function update(updatedMed) {
+    setMeds(meds =>
+      meds.map(med =>
+        med.id === updatedMed.id ? updatedMed : med,
+      ),
+    );
+  }
+
+  /** Delete a med by id */
+  function remove(id) {
+    setMeds(meds => meds.filter(med => med.id !== id));
+  }
+
+  
+}
