0a17c60d572c0cbbec8174f9551a01a35acb3771
Philip Dimaano <pdimaano7@gmail.com>
2023-07-29 17:36:42 -0700

    added initial import statements and docstring

---
+++ src/NavBar.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./NavBar.css";
+import { Link, NavLink } from "react-router-dom";
+import { Nav, NavItem } from "reactstrap";
+
+/** Top navigation bar for site */
