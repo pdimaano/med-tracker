fdd3a1ad979ffd3c8c1521866807e1d6d2c73323
Philip Dimaano <pdimaano7@gmail.com>
2023-07-29 17:43:40 -0700

    added links for med form and daily medications

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,3 +4,45 @@ import { Link, NavLink } from "react-router-dom";
 import { Nav, NavItem } from "reactstrap";
 
 /** Top navigation bar for site */
+
+function NavBar() {
+  return (
+    <nav className="Navigation navbar navbar-expand-md">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          Daily Medications
+        </Link>
+        <Nav className="ms-auto" navbar>
+          <NavItem>
+            <NavLink to="/add" className="me-0">
+              Add Med
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/sunday">Sunday</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/monday">Monday</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/tuesday">Tuesday</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/wednesday">Wednesday</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/thursday">Thursday</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/friday">Friday</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink to="/saturday">Saturday</NavLink>
+          </NavItem>
+        </Nav>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
