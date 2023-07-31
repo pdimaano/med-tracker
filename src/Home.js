6f016de2025769f7775cb3bf58b09dc94ebc20df
Philip Dimaano <pdimaano7@gmail.com>
2023-07-31 12:24:51 -0700

    added function, title and description for home page

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,28 @@
 import React from "react";
-import { Card, CardBody, CardTitle, CardText } from "reactstratp";
+import { Card, CardBody, CardTitle, CardText } from "reactstrap";
 
 /** Home Page
  *
+ *  Props: none
  */
 
+function Home() {
+  return (
+    <section className="col-md-8">
+      <Card>
+        <CardBody className="text-center">
+          <CardTitle>
+            <p className="fw-bold" style={{ fontSize: "1.5em" }}>
+              Your Daily Medication Tracker
+            </p>
+          </CardTitle>
+          <CardText>
+            Use the navigation bar at the top.
+          </CardText>
+        </CardBody>
+      </Card>
+    </section>
+  );
+}
+
+export default Home;
