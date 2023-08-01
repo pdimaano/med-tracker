30d049fa2cbb4bd1354c3d064c8e007068895e70
Philip Dimaano <pdimaano7@gmail.com>
2023-08-01 15:52:33 -0700

    included function to set state, fetch meds, change loading status, add medications and display application

--- src/App.js
+++ src/App.js
@@ -18,3 +18,70 @@ import MedForm from "./MedForm";
  *  - isLoading: boolean, has data loaded yet?
  *
  */
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [meds, setMeds] = useState({
+    sunday: [],
+    monday: [],
+    tuesday: [],
+    wednesday: [],
+    thursday: [],
+    friday: [],
+    saturday: [],
+  });
+
+  /** Load data from backend */
+
+  useEffect(function fetchMedsOnMount() {
+    async function getAllMeds() {
+      const sundays = await fetchMeds("sunday");
+      const mondays = await fetchMeds("monday");
+      const tuesdays = await fetchMeds("tuesday");
+      const wednesdays = await fetchMeds("wednesday");
+      const thursdays = await fetchMeds("thursday");
+      const fridays = await fetchMeds("friday");
+      const saturdays = await fetchMeds("saturday");
+      setMeds({
+        sundays,
+        mondays,
+        tuesdays,
+        wednesdays,
+        thursdays,
+        fridays,
+        saturdays,
+      });
+      setIsLoading(false);
+    }
+
+    getAllMeds();
+  }, []);
+
+  /** Call API to add med for specific day; update state */
+
+  async function addMed(day, { name, description }) {
+    let id = slugify(name, { lower: true });
+    let objData = { id, name, description };
+    await addMedApi(day, objData);
+    setMeds((m) => ({
+      ...m,
+      [day]: [...m[day], objData],
+    }));
+  }
+
+  /** Show app frame, navbar and routes */
+
+  let {
+    sundays,
+    mondays,
+    tuesdays,
+    wednesdays,
+    thursdays,
+    fridays,
+    saturdays,
+  } = meds;
+
+  if (isLoading) {
+    return <p>Loading...</p>
+  }
+}
