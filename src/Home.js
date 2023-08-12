4245c859499231dfb7897a36a54e0596b16147b6
Philip Dimaano <pdimaano7@gmail.com>
2023-08-12 14:31:40 -0700

    changed text for homepage

--- src/Home.js
+++ src/Home.js
@@ -13,11 +13,11 @@ function Home() {
         <CardBody className="text-center">
           <CardTitle>
             <p className="fw-bold" style={{ fontSize: "1.5em" }}>
-              Your Daily Medication Tracker
+              Daily Medication Tracker
             </p>
           </CardTitle>
           <CardText>
-            Use the navigation bar at the top.
+            For every day of the week
           </CardText>
         </CardBody>
       </Card>
