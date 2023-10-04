e0331cddde6bd2e28bf7dd71fe648410f7b34915
Philip Dimaano <pdimaano7@gmail.com>
2023-10-03 17:02:19 -0700

    additional code for testing form submission

--- src/MedForm.test.js
+++ src/MedForm.test.js
@@ -10,3 +10,29 @@ it("renders without crashing", async function () {
   );
   expect(asFragment()).toMatchSnapshot();
 });
+
+it("calls addItem with correct inputs upon save", async function () {
+  const handleSave = jest.fn();
+  const { container } = render(
+    <MemoryRouter initialEntries={["/add"]}>
+      <MedForm addItem={handleSave} />
+    </MemoryRouter>
+  );
+  fireEvent.change(screen.getAllByLabelText("Day"), {
+    target: { value: "Sunday" },
+  });
+  fireEvent.change(screen.getAllByLabelText("Name"), {
+    target: { value: "test name" },
+  });
+  fireEvent.change(screen.getAllByLabelText("Description"), {
+    target: { value: "test description" },
+  });
+  const button = container.querySelector("button");
+
+  fireEvent.click(button);
+  expect(handleSave).toHaveBeenCalled();
+  expect(handleSave).toHaveBeenCalledWith("Sunday", {
+    name: "test name",
+    description: "test description",
+  });
+});
