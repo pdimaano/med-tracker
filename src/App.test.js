5e5ca144014fcd360db900710ab5619e60be16e4
Philip Dimaano <pdimaano7@gmail.com>
2023-10-05 12:14:03 -0700

    initial tests for snapshot and routes

--- src/App.test.js
+++ src/App.test.js
@@ -8,3 +8,33 @@ api.fetchMeds = jest.fn();
 api.addMed = jest.fn();
 
 const WELCOME_TEXT = "Daily Medication Tracker";
+
+describe("Med Tracker routes", function () {
+  beforeEach(() => {
+    api.fetchMeds.mockReturnValue([]);
+    api.addMed.mockReturnValue([]);
+  });
+
+  it("renders without crashing", async function () {
+    const { getByText } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>
+    );
+    await waitFor(() => {
+      screen.getByText(WELCOME_TEXT);
+    });
+  });
+
+  it("renders the home snapshot", async function () {
+    const { asFragment, debug } = render(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>
+    );
+    await waitFor(() => screen.getByText(WELCOME_TEXT));
+    expect(api.fetchMeds).toHaveBeenCalledTimes(7);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
