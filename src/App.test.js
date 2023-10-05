0ae136463bc6a31440f89de7a7a46fc1c2c2c7f9
Philip Dimaano <pdimaano7@gmail.com>
2023-10-05 12:06:14 -0700

    initial setup with statements and mock functions

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import * as api from "./api";
+import React from "react";
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { MemoryRouter } from "react-router-dom";
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+api.fetchMeds = jest.fn();
+api.addMed = jest.fn();
+
+const WELCOME_TEXT = "Daily Medication Tracker";
