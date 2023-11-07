1f3b3458980bec3b6fb1446a94a59847cf2a309b
Philip Dimaano <pdimaano7@gmail.com>
2023-11-07 12:45:10 -0800

    added test data

--- src/Home.test.js
+++ src/Home.test.js
@@ -1,3 +1,20 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import Home from "./Home";
+
+const testData = {
+  sunday: [
+    {
+      id: "sunday med",
+      name: "test1",
+      description: "test one description",
+    },
+  ],
+  monday: [
+    {
+      id: "monday med",
+      name: "test2",
+      description: "test two description",
+    },
+  ],
+};
