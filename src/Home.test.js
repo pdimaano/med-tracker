1c4b522511a9ca7f341a6615ab159303d2899a36
Philip Dimaano <pdimaano7@gmail.com>
2023-11-10 11:45:21 -0800

    removed test data and added snapshot test

--- src/Home.test.js
+++ src/Home.test.js
@@ -2,25 +2,17 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Home from "./Home";
 
-const testData = {
-  sunday: [
-    {
-      id: "sunday med",
-      name: "test1",
-      description: "test one description",
-    },
-  ],
-  monday: [
-    {
-      id: "monday med",
-      name: "test2",
-      description: "test two description",
-    },
-  ],
-};
-
 describe("Home component", function () {
   it("renders the home component", function () {
     render(<Home />);
   });
+
+  it("matches a snapshot", function () {
+    const { getByText, asFragment } = render(
+      <Home />
+    );
+    getByText(/Daily Medication/);
+
+    expect(asFragment()).toMatchSnapshot();
+  })
 })
