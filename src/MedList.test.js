b02ed300fcc0b39f60dd2005487cd7e89c3bee8d
Philip Dimaano <pdimaano7@gmail.com>
2023-11-16 11:32:31 -0800

    snapshot test for med list

--- src/MedList.test.js
+++ src/MedList.test.js
@@ -2,3 +2,12 @@ import { screen, fireEvent, render, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import MedList from "./MedList";
 
+it("renders without crashing for med list", async function () {
+  const { asFragment } = render(
+    <MemoryRouter initialEntries={["/monday"]}>
+      <MedList day="monday" meds={[{ id: 0, name: "test med" }]} />
+    </MemoryRouter>
+  );
+  screen.getByText("test med");
+  expect(asFragment()).toMatchSnapshot();
+});
