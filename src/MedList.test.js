ac6ffa2a26a064b962da9c8d9592b189bc42f363
Philip Dimaano <pdimaano7@gmail.com>
2023-11-20 12:13:42 -0800

    added tests for different days

--- src/MedList.test.js
+++ src/MedList.test.js
@@ -2,12 +2,22 @@ import { screen, fireEvent, render, waitFor } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import MedList from "./MedList";
 
-it("renders without crashing for med list", async function () {
+it("renders without crashing for monday meds", async function () {
   const { asFragment } = render(
     <MemoryRouter initialEntries={["/monday"]}>
-      <MedList day="monday" meds={[{ id: 0, name: "test med" }]} />
+      <MedList day="monday" meds={[{ id: 0, name: "monday med" }]} />
     </MemoryRouter>
   );
-  screen.getByText("test med");
+  screen.getByText("monday med");
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("renders without crashing for friday meds", async function () {
+  const { asFragment } = render(
+    <MemoryRouter initialEntries={["/friday"]}>
+      <MedList day="friday" meds={[{ id: 0, name: "friday med" }]} />
+    </MemoryRouter>
+  );
+  screen.getByText("friday med");
   expect(asFragment()).toMatchSnapshot();
 });
